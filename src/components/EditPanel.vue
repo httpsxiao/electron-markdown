8a761b6becf317ea1468fca3ee7cbad62af91bcb
httpsxiao <day_clear@163.com>
2020-06-08 15:04:36 +0800

    update preview

--- src/components/EditPanel.vue
+++ src/components/EditPanel.vue
@@ -1,23 +1,27 @@
 <template>
   <div class="edit-panel">
-    <textarea class="edit" v-model="content"></textarea>
-    <div class="preview" v-text="previewContent"></div>
+    <textarea class="edit" v-model="currentArticle.content"></textarea>
+    <div class="preview" v-html="previewContent"></div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Watch, Vue } from 'vue-property-decorator'
+import Article from '../data/Article'
 import marked from 'marked'
 
 @Component
 export default class MenuBar extends Vue {
-  content = '# aaa'
-
-  @Prop() private msg!: string
+  @Prop() currentArticle!: Article
 
   get previewContent () {
-    return marked(this.content)
+    return marked(this.currentArticle.content)
   }
+
+  // update (e) {
+    // const value = e.currentTarget.value
+    // console.log(value)
+  // }
   // @Watch('content', { immediate: true })
   // contentChanged (val) {}
 }
