db0803b37b879f4886efda42f02b06c810e0bb59
httpsxiao <day_clear@163.com>
2020-06-24 19:45:45 +0800

    add devtools

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { remote } from 'electron'
+
+
+window.addEventListener('keydown', e => {
+  const { altKey, ctrlKey, metaKey, keyCode } = e
+  if (altKey && ctrlKey && metaKey && keyCode === 68){
+    const currentWebContents = remote.getCurrentWebContents()
+    currentWebContents && currentWebContents.toggleDevTools()
+    e.preventDefault()
+  }
+}, false)
+
+window.document.title = 'markdown 编辑器'
 
 Vue.config.productionTip = false
 
