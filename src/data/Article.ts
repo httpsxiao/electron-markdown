24fbf1e0b4dc33a4c79586368634e34ac7a772a9
httpsxiao <day_clear@163.com>
2020-07-09 20:24:38 +0800

    update article

--- src/data/Article.ts
+++ src/data/Article.ts
@@ -6,12 +6,14 @@ export default class Artical {
   created: string
   updated: string
   content: string
+  change: boolean
 
   constructor (content = '', filePath = '', id = '') {
     const time = '' + Date.now()
     this.id = id || time
     this.created = time
     this.updated = time
+    this.change = false
     this.content = content
     this.filePath = filePath
   }
