a14d8ea728eb7b1a18f0a36cca41565dad993b39
httpsxiao <day_clear@163.com>
2020-07-26 17:10:54 +0800

    update config

--- vue.config.js
+++ vue.config.js
@@ -1,43 +1,47 @@
 module.exports = {
   pluginOptions: {
     electronBuilder: {
-      appId: 'com.github.httpsxiao.electron-markdown',
-      productName: 'electron markdown',
-      win: {
-        icon: 'public/favicon.ico',
-        target: 'nsis',
-        arch: [
-          'ia32'
-        ]
-      },
-      mac: {
-        icon: 'public/icon.icns'
-      },
-      linux: {
-        icon: 'public/favicon.ico'
-      },
-      dmg: {
-        contents: [
-          {
-            x: 300,
-            y: 200,
-            type: 'link',
-            path: '/Applications'
-          },
-          {
-            x: 130,
-            y: 200,
-            type: 'file'
-          }
-        ]
-      },
-      nsis: {
-        oneClick: false,
-        allowElevation: true,
-        allowToChangeInstallationDirectory: true,
-        installerHeaderIcon: 'public/favicon.ico',
-        createDesktopShortcut: true,
-        shortcutName: 'electron markdown'
+      builderOptions: {
+        appId: "com.github.httpsxiao.electron-markdown",
+        productName: "ElectronMarkdown",
+        win: {
+          icon: "public/favicon.ico",
+          target: [{
+            target: "nsis",
+            arch: ["ia32"]
+          }]
+        },
+        mac: {
+          target: "dmg",
+          icon: "public/icon.icns"
+        },
+        linux: {
+          icon: "public/favicon.ico"
+        },
+        dmg: {
+          contents: [
+            {
+              x: 300,
+              y: 200,
+              type: "link",
+              path: "/Applications",
+            },
+            {
+              x: 130,
+              y: 200,
+              type: "file",
+            }
+          ]
+        },
+        asar: false,
+        nsis: {
+          oneClick: false,
+          allowElevation: true,
+          allowToChangeInstallationDirectory: true,
+          installerHeaderIcon: "public/favicon.ico",
+          createDesktopShortcut: true,
+          shortcutName: "electron markdown"
+        }
       }
     }
   }
